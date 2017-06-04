c0fb058052e99a0c97d4aa459e25887d85719011
Alejandro <Alejandro@Lenovo-PC>
2017-06-04 13:56:55 +0200

    2

--- examen taxista/src/GUI/Login.java
+++ examen taxista/src/GUI/Login.java
@@ -3,10 +3,15 @@ package GUI;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JButton;
+import javax.swing.JRadioButton;
+import java.awt.Font;
 
 public class Login {
 
 	protected JFrame frmLogIn;
+	private JButton jbviajero;
+	private JButton jbcaja;
 
 	/**
 	 * Create the application.
@@ -20,8 +25,19 @@ public class Login {
 	 */
 	private void initialize() {
 		frmLogIn = new JFrame();
-		frmLogIn.setBounds(100, 100, 450, 300);
+		frmLogIn.setTitle("Login");
+		frmLogIn.setBounds(100, 100, 499, 366);
 		frmLogIn.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frmLogIn.getContentPane().setLayout(null);
+		
+		jbviajero = new JButton("Subida del viajero");
+		jbviajero.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		jbviajero.setBounds(88, 162, 135, 43);
+		frmLogIn.getContentPane().add(jbviajero);
+		
+		jbcaja = new JButton("Caja del dia");
+		jbcaja.setFont(new Font("Times New Roman", Font.BOLD, 13));
+		jbcaja.setBounds(253, 162, 135, 43);
+		frmLogIn.getContentPane().add(jbcaja);
 	}
-
 }
